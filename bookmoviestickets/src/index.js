557710cc67d749a778b240c362fc3112dcdab845
Prakash-453 <alwaysprakash453@gmail.com>
2025-01-13 11:17:48 +0530

    Book Movie Tickets

--- bookmoviestickets/src/index.js
+++ bookmoviestickets/src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 // import Seats1 from "./Components/SeatsComponent/Seats1";
-import Seats2 from "./Components/SeatsComponent/Seats2";
+// import Seats2 from "./Components/SeatsComponent/Seats2";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <Seats1/> */}
-    <Seats2/>
+    {/* <Seats2/> */}
   </React.StrictMode>
 );
